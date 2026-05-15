e53b17d182a6f54a488b8c9272696e75dc12d197
SuperSlayQueen <150417843+SuperSlayQueen@users.noreply.github.com>
2026-05-15 13:09:03 +0300

    Delete dating_bot directory

--- dating_bot/src/backend/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Models package
-from src.backend.models.models import User, Profile, ProfilePhoto, Like, Match, Rating, Event
-
-__all__ = ["User", "Profile", "ProfilePhoto", "Like", "Match", "Rating", "Event"]
